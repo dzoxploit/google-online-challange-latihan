e3d70af917ee6c3fef43cfc14c0fb9f79ff8ac4f
Didin Yahya <didinnuryahya@gmail.com>
2021-10-09 19:09:24 +0700

    third commit

---
+++ deretsederhana.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int deretsederhana()
+{
+    int i,m;
+    cout<<"Program Cihuy";
+   i = 0;
+   m = 0;
+   while(i < 9){
+       m = i * i;
+       cout<<m<<"\n";
+       i = i + 1;
+   }
+}
